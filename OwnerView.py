ed6b6f18a3317c8bc8e9983de24a8eb689c2b915
Nisha Bevans <nisha.chavez@yahoo.com>
2024-09-28 16:45:01 -0500

    Updated Code

--- OwnerView.py
+++ OwnerView.py
@@ -58,7 +58,7 @@ def main():
     salon = Salon()
 
     while True:
-        print("\nEyelash Salon Management")
+        print("\nIllenium Lash Salon Management")
         print("1. Add Customer")
         print("2. View Customer Recent Visit")
         print("3. Schedule Appointment")
